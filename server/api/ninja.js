4b63e6d4bea6569c6244716c2675f8a7a075243e
Aniket Aryamane <aryamane.aniket@gmail.com>
2024-03-16 11:01:51 +0530

    Lesson 12 complete - 3

--- server/api/ninja.js
+++ server/api/ninja.js
@@ -1,9 +1,9 @@
 export default defineEventHandler(async (e) => {
 	const { name } = getQuery(e)
 	const { age, surname } = await readBody(e)
-	const API_KEY = import.meta.env.VITE_API_KEY
+	const config = useRuntimeConfig()
 
-	const { data } = await $fetch(`https://api.currencyapi.com/v3/currencies?apikey=${API_KEY}`)
+	const { data } = await $fetch(`https://api.currencyapi.com/v3/currencies?apikey=${config.currencyKey}`)
 
 	return {
 		message: `${name} ${surname} - ${age} yrs. old !!`,
