ce02697a22e56548e81514131ed2c300e6f001cd
Aniket Aryamane <aryamane.aniket@gmail.com>
2024-03-19 03:35:08 +0530

    Lesson 12 complete - 8

--- server/api/ninja.js
+++ server/api/ninja.js
@@ -1,7 +1,7 @@
 export default defineEventHandler(async (e) => {
     const { name } = getQuery(e);
     const { age, surname } = await readBody(e);
-    const { currencyAPIKey } = useRuntimeConfig(e);
+    const currencyAPIKey = useRuntimeConfig(e).currencyAPIKey;
 
     const { data } = await $fetch(
         `https://api.currencyapi.com/v3/currencies?apikey=${currencyAPIKey}`
